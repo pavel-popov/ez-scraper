30d379422c1984b60a1b2233cd239371e48b4146
Pavel Popov <pavelpopov@outlook.com>
2016-11-27 22:24:32 +0300

    Add 2016-10-02 and 2016-11-27 data

---
+++ output/2016-10-02.sql
@@ -0,0 +1,25 @@
+building,floor,height,rooms,rowid,square,price
+insert into ez(dt, flat_id, building, floor, height, rooms, square, price)
+select date'2016-10-02' dt, flat_id, building, floor, height, rooms, square, price from (
+select null flat_id, 0 building, 0 floor, 0 height, 0 rooms, 0 square, 0 price from dual)
+where flat_id is not null;
+building,floor,height,rooms,rowid,square,price
+insert into ez(dt, flat_id, building, floor, height, rooms, square, price)
+select date'2016-10-02' dt, flat_id, building, floor, height, rooms, square, price from (
+select null flat_id, 0 building, 0 floor, 0 height, 0 rooms, 0 square, 0 price from dual)
+where flat_id is not null;
+building,floor,height,rooms,rowid,square,price
+insert into ez(dt, flat_id, building, floor, height, rooms, square, price)
+select date'2016-10-02' dt, flat_id, building, floor, height, rooms, square, price from (
+select null flat_id, 0 building, 0 floor, 0 height, 0 rooms, 0 square, 0 price from dual)
+where flat_id is not null;
+building,floor,height,rooms,rowid,square,price
+insert into ez(dt, flat_id, building, floor, height, rooms, square, price)
+select date'2016-10-02' dt, flat_id, building, floor, height, rooms, square, price from (
+select null flat_id, 0 building, 0 floor, 0 height, 0 rooms, 0 square, 0 price from dual)
+where flat_id is not null;
+building,floor,height,rooms,rowid,square,price
+insert into ez(dt, flat_id, building, floor, height, rooms, square, price)
+select date'2016-10-02' dt, flat_id, building, floor, height, rooms, square, price from (
+select null flat_id, 0 building, 0 floor, 0 height, 0 rooms, 0 square, 0 price from dual)
+where flat_id is not null;
--- scrape.sh
+++ scrape.sh
@@ -11,6 +11,9 @@ echo -n "Run scraper... "
 ./run_scraper.sh 2 > output/$dt-2.json
 ./run_scraper.sh 3 > output/$dt-3.json
 ./run_scraper.sh 4 > output/$dt-4.json
+./run_scraper.sh 5 > output/$dt-5.json
+./run_scraper.sh 6 > output/$dt-6.json
+./run_scraper.sh 7 > output/$dt-7.json
 echo "Done"
 
 echo -n "Removing Warning message... "
@@ -19,6 +22,9 @@ sed '1d' output/$dt-1.json > tmpfile; mv tmpfile output/$dt-1.json
 sed '1d' output/$dt-2.json > tmpfile; mv tmpfile output/$dt-2.json
 sed '1d' output/$dt-3.json > tmpfile; mv tmpfile output/$dt-3.json
 sed '1d' output/$dt-4.json > tmpfile; mv tmpfile output/$dt-4.json
+sed '1d' output/$dt-5.json > tmpfile; mv tmpfile output/$dt-5.json
+sed '1d' output/$dt-6.json > tmpfile; mv tmpfile output/$dt-6.json
+sed '1d' output/$dt-7.json > tmpfile; mv tmpfile output/$dt-7.json
 echo "Done"
 
 echo -n "Converting to csv... "
@@ -28,6 +34,9 @@ echo "building,floor,height,rooms,rowid,square,price" > output/$dt.csv
 ./to_csv.sh output/$dt-2.json >> output/$dt.csv
 ./to_csv.sh output/$dt-3.json >> output/$dt.csv
 ./to_csv.sh output/$dt-4.json >> output/$dt.csv
+./to_csv.sh output/$dt-5.json >> output/$dt.csv
+./to_csv.sh output/$dt-6.json >> output/$dt.csv
+./to_csv.sh output/$dt-7.json >> output/$dt.csv
 echo "Done"
 
 echo -n "Converting to sql "
@@ -36,6 +45,9 @@ echo -n "Converting to sql "
 ./to_sql.sh output/$dt-2.json $dt >> output/$dt.sql
 ./to_sql.sh output/$dt-3.json $dt >> output/$dt.sql
 ./to_sql.sh output/$dt-4.json $dt >> output/$dt.sql
+./to_sql.sh output/$dt-5.json $dt >> output/$dt.sql
+./to_sql.sh output/$dt-6.json $dt >> output/$dt.sql
+./to_sql.sh output/$dt-7.json $dt >> output/$dt.sql
 echo "Done"
 
 echo -n "Copying to output.csv... "
