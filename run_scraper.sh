9038b067b382ce87f0fe9401c393bce23509eb92
Pavel Popov <pavel.popov@onefactor.com>
2017-03-20 01:28:12 +0300

    Fix hyper volumes usage; Add data for 2017-03-20

--- run_scraper.sh
+++ run_scraper.sh
@@ -5,19 +5,25 @@ IFS=$'\n\t'
 output=$1
 dir=${2:-"$(pwd)"}
 
-echo "Processing in $dir writing output in ez-scraper/$output"
+volume=ez-scraper
 
+echo "Creating volume $volume"
+hyper volume create --name $volume
+
+echo "Processing in $dir writing output in $volume/$output"
 hyper run --rm --name ez-scraper \
     -v $dir/ez.js:/data/ez.js \
-    -v ez-scraper:/volume \
+    -v $volume:/volume \
     schmooser/casperjs \
     casperjs /data/ez.js --output=/volume/$output
 
 echo "Scraping finished, reading output file $output"
-
 hyper run --rm --name ez-scraper-reader \
     -v ez-scraper:/volume \
     alpine \
     cat /volume/$output | jq '.' > output/$output
 
 echo "Building processing completed"
+
+echo "Removing volume $volume"
+hyper volume rm $volume
