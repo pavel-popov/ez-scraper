ca494f7147f9ac68a32427e2af402cddd785eb7a
Pavel Popov <pavel.popov@onefactor.com>
2017-04-08 00:59:46 +0300

    Add 2017-04-08 data

--- run_docker.sh
+++ run_docker.sh
@@ -1,6 +1,5 @@
 docker run -t -i \
     --name casperjs \
     -v ~/Projects/etalonzvezda-scraper:/data \
-    -v /srv/btsync/Sync:/sync \
     schmooser/casperjs \
     /bin/bash
--- sell_dynamics.sh
+++ sell_dynamics.sh
@@ -2,6 +2,8 @@ IFS=$'\n'
 for row in $(git lol)
 do
     commit=$(echo $row | cut -f2 -d" ")
+    date=$(git show -s --format=%ci $commit)
     num_flats=$(git show $commit:output/output.csv 2> /dev/null | wc -l)
-    printf "%80s %d\n" "$row" $num_flats
+    #printf "%80s      %s     %d\n" "$row" $date $num_flats
+    printf "%s %d\n" $date $num_flats
 done
